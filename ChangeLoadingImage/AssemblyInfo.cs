35f38b6fcdaa1343f0a629a1db7fc09553d20d0c
githubpermutation <githubpermutation@users.noreply.github.com>
2015-04-24 16:16:44 +0200

    Change assembly version for release

--- ChangeLoadingImage/AssemblyInfo.cs
+++ ChangeLoadingImage/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("ChangeLoadingImage_beta")]
+[assembly: AssemblyTitle("ChangeLoadingImage1.0")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
